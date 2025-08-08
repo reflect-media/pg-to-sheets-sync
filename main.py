96861c3729dbfeff7075db5fd29b93c18318b366
reflect-media <tal@reflect-media.co.il>
2025-08-08 13:19:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-def main(request):
-    import psycopg2
-    import gspread
-    from oauth2client.service_account import ServiceAccountCredentials
+from flask import Flask, request
+import psycopg2
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import os
 
-    # חיבור ל־PostgreSQL
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def sync_data():
+    # חיבור ל-PostgreSQL
     conn = psycopg2.connect(
         host='rtngplsadmin40.data-driven.media',
         port=5432,
         dbname='clients_managment',
         user='looker_mediaforest',
-        password='<< הסיסמה שלך כאן >>'
+        password=os.environ['PG_PASS']
     )
-
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM campaign_summary_last_7_days_new")
     rows = cursor.fetchall()
@@ -19,15 +23,16 @@ def main(request):
     cursor.close()
     conn.close()
 
-    # חיבור ל־Google Sheets
+    # חיבור ל-Google Sheets
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
     client = gspread.authorize(creds)
-
-    sheet = client.open("<< שם הגיליון >>").worksheet("<< שם הטאב >>")
+    sheet = client.open("קמפיינים יומיים מבסיס נתוני רייטינג פלוס v24.7.25.13.07").sheet1
     sheet.clear()
     sheet.insert_row(headers, 2)
     sheet.insert_rows(rows, 3)
 
-    return 'Success'
+    return "Success", 200
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
